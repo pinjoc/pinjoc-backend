8a02783ed321963c145468b2453b835de84fc26c
Ardi Sasongko <ardisasongko002@gmail.com>
2025-02-25 22:12:24 +0700

    feat: fix cors and tokenized

--- cmd/api/api.config.go
+++ cmd/api/api.config.go
@@ -30,7 +30,7 @@ func NewApp(config AppConfig) *Application {
 func (a *Application) RegisterRoute() {
 	a.app.Use(recover.New())
 	a.app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://dev-pinjoc.netlify.app",
+		AllowOrigins:     "*",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		ExposeHeaders:    "Content-Length",
